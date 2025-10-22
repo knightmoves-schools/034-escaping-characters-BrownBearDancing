1bbdcc741f69df3a193d6caeb211ee50a671cdf1
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-10-22 11:56:13 -0500

    check this one

---
+++ index.js
@@ -0,0 +1,8 @@
+let introduction= 'Dear Mr. Smith, \n' +
+"\tThank you for your time and the \"trouble\" we caused you.\n" +
+  'Next time I hope it is a much \'better\' visit.\n\n' +
+  'Thanks again, \n'+
+  'Mary Jo \:)\'
+
+document.getElementById("result").innerHTML = introduction;
+
